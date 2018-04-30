bd31dad223597da0f0a443d9fe5f48ebaaf5efa5
Carlos Maita <Carlosmaita2009@gmail.com>
2018-04-30 16:29:49 -0400

    Add files via upload

---
+++ Common/contador.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$archivo = "contador.txt"; 
+$contador = 0; 
+
+$fp = fopen($archivo,"r"); 
+$contador = fgets($fp, 26); 
+fclose($fp); 
+
+++$contador; 
+
+$fp = fopen($archivo,"w+"); 
+fwrite($fp, $contador, 26); 
+fclose($fp); 
+
+echo "Esta página ha sido visitada $contador veces"; 
+
